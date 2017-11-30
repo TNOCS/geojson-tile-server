561296013e2bea5740d421631bb3244536111c26
Erik Vullings <erik.vullings@gmail.com>
2017-11-30 21:12:25 +0100

    Added declaration to import json files.

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -4,3 +4,14 @@ declare module 'vt-pbf' {
   };
   export = vtpbf;
 }
+
+declare module '*.json' {
+  const foo: {
+    name: string;
+    version: string;
+    author: string;
+    license: string;
+    description: string;
+  };
+  export = foo;
+}
