3dc0a112d6301e66d8eeb613744465303e343327
Erik Vullings <erik.vullings@gmail.com>
2019-06-15 16:47:07 +0200

    Close #1 Fix MVT output for Mapbox GL consumption

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import * as path from 'path';
 import * as express from 'express';
 import * as cors from 'cors';
 import * as vtpbf from 'vt-pbf';
+import * as stream from 'stream';
 import { Request, Response } from 'express';
 import { GeometryObject } from 'geojson';
 import { createTileIndex, findGeojsonFilesInFolder } from './utils';
@@ -11,18 +12,29 @@ import { IVectorTile, toFeatureCollection } from './vt2geojson';
 /** GeojsonVT extent option */
 const extent = 4096;
 
+const emptyResponse = { tile: undefined, x: 0, y: 0, z: 0 };
+
 const startService = async (filenames: string | string[], options: ICommandOptions) => {
-  if (typeof filenames === 'string') { filenames = [filenames]; }
+  if (typeof filenames === 'string') {
+    filenames = [filenames];
+  }
 
   const tileIndexes: { [key: string]: any } = {};
   let countFiles = filenames.length;
   filenames.forEach(async f => {
     const layerName = path.basename(f).replace(path.extname(f), '');
     console.log(`Processing layer ${layerName}...`);
-    const tileIndex = await createTileIndex(f, { extent });
+    const tileIndex = await createTileIndex(f, {
+      extent,
+      maxZoom: options.maxZoom,
+      generateId: options.generatedId,
+      promoteId: options.promoteId,
+    });
     tileIndexes[layerName] = tileIndex;
     countFiles--;
-    if (countFiles <= 0) { console.log('Finished loading the layers.'); }
+    if (countFiles <= 0) {
+      console.log('Finished loading the layers.');
+    }
   });
 
   const httpPort = options.port || process.env.PORT || 8123;
@@ -30,47 +42,70 @@ const startService = async (filenames: string | string[], options: ICommandOptio
   app.use(cors());
   app.use(express.static(process.env.PUBLIC_FOLDER || './public'));
 
+  const extensions = ['geojson', 'mvt (for MapBox GL, use source-layer: "all")', 'vt'];
+
+  const ulList = (l: string) => '<ul>' + extensions.map(ext => `<li>${l}/{z}/{x}/{y}.${ext}</li>`).join('\n') + '</ul>';
+
   const send404 = (res: Response) => {
-    const availableLayers = Object.keys(tileIndexes).join(', ');
-    res.status(404).send(`Available layers are ${availableLayers}. Please create a query using LAYERNAME/z/x/y.ext, where ext is geojson, vt or mvt.`);
+    const availableLayers = Object.keys(tileIndexes);
+    const list = availableLayers
+      .sort()
+      .map(l => `<ol><li>${ulList(l)}</li></ol>`)
+      .join('\n');
+    res.status(404).send(
+      `<h1>Layer not found</h1>
+        <p>Available layers are:
+        ${list}
+        </p>`
+    );
   };
 
-  app.get('/', (req, res) => send404(res));
-
-  app.get('/:layer/:z/:x/:y.geojson', (req, res) => {
+  const getTile = (req: Request, res: Response) => {
     const layer = req.params['layer'];
-    if (!tileIndexes.hasOwnProperty(layer)) { send404(res); }
+    if (!tileIndexes.hasOwnProperty(layer)) {
+      send404(res);
+      return emptyResponse;
+    }
     const z = +req.params['z'];
     const x = +req.params['x'];
     const y = +req.params['y'];
     const tile = tileIndexes[layer].getTile(z, x, y);
-    if (!tile || !tile.features) { return res.json({}); }
+    if (!tile || !tile.features) {
+      res.json({});
+    }
+    return { tile, x, y, z };
+  };
+
+  app.get('/', (_, res) => send404(res));
+
+  app.get('/:layer/:z/:x/:y.geojson', (req, res) => {
+    const { tile, x, y, z } = getTile(req, res);
+    if (!tile || !tile.features) {
+      return;
+    }
     const vectorTiles = tile.features as IVectorTile[];
     res.json(toFeatureCollection(vectorTiles, x, y, z, extent));
   });
 
   app.get('/:layer/:z/:x/:y.vt', (req, res) => {
-    const layer = req.params['layer'];
-    if (!tileIndexes.hasOwnProperty(layer)) { send404(res); }
-    const z = +req.params['z'];
-    const x = +req.params['x'];
-    const y = +req.params['y'];
-    const tile = tileIndexes[layer].getTile(z, x, y);
-    if (!tile || !tile.features) { return res.json({}); }
+    const { tile } = getTile(req, res);
+    if (!tile || !tile.features) {
+      return;
+    }
     const vectorTiles = tile.features as IVectorTile[];
     res.json(vectorTiles);
   });
 
   app.get('/:layer/:z/:x/:y.mvt', (req, res) => {
-    const layer = req.params['layer'];
-    if (!tileIndexes.hasOwnProperty(layer)) { send404(res); }
-    const z = +req.params['z'];
-    const x = +req.params['x'];
-    const y = +req.params['y'];
-    const tile = tileIndexes[layer].getTile(z, x, y);
-    if (!tile || !tile.features) { return res.json({}); }
-    const buff = vtpbf.fromGeojsonVt({ 'geojsonLayer': tile });
-    res.send(buff);
+    const { tile } = getTile(req, res);
+    if (!tile || !tile.features) {
+      return;
+    }
+    /** Notice that I set the source-layer (for Mapbox GL) to all */
+    const buff = vtpbf.fromGeojsonVt({ all: tile });
+    const buffStream = new stream.PassThrough();
+    buffStream.end(buff);
+    buffStream.pipe(res);
   });
 
   app.listen(httpPort, () => console.info(`Tile service is listening on port ${httpPort}`));
